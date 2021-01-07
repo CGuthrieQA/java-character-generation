146b732353e94aa24e6facb05803ffd6b79f4282
CGuthrieQA <CGuthrie@QA.com>
2021-01-07 08:34:58 +0000

    initial commit

---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,71 @@
+// This rolls 3d6 down the line for D&D statistics.
+
+package com.qa.main;
+
+import java.util.Random; // import random
+
+public class Runner {
+	
+	// roll a D6
+	public static int rollD6() {
+		
+		// create new random instance of class Random
+		Random ran = new Random();
+		
+		// random number between 0 and 5 (+1)
+		int roll = ran.nextInt(6) + 1;
+		
+		// return value of roll
+		return roll;
+		
+	}
+	
+	// roll 3 D6
+	public static int roll3D6() {
+		
+		// total of rolls
+		int total = 0;
+		
+		// i variable to loop
+		int i;
+		
+		// loop 3 times (once per d6)
+		for ( i = 0; i < 3; i++ ) {
+			
+			// add d6 to the total
+			total += rollD6();
+			
+		}
+		
+		//return the total number
+		return total;
+		
+	}
+	
+	// Generate a character
+	public static void charGen() {
+		
+		// statistics variable
+		String[] stats = {"STR", "DEX", "CON", "INT", "WIS", "CHA"};
+		
+		// i variable to loop
+		int i;
+		
+		// loop through statistics array 
+		for (i = 0; i < stats.length; i ++) {
+			
+			//print the array item followed by a space followed by the result of rolling 3d6
+			System.out.println(stats[i] + " " + roll3D6());
+			
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		// do the thing!
+		charGen();
+		
+	}
+
+}
